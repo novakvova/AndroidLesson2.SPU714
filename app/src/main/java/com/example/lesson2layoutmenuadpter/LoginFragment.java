db140bd2a99bfbaf39b4b94ad29d8f42402e49bb
novakvova <novakvova@gmail.com>
2020-03-21 11:34:25 +0200

    Add progress bar loading

--- app/src/main/java/com/example/lesson2layoutmenuadpter/LoginFragment.java
+++ app/src/main/java/com/example/lesson2layoutmenuadpter/LoginFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.lesson2layoutmenuadpter.utils.CommonUtils;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -40,6 +41,7 @@ public class LoginFragment extends Fragment {
 
             @Override
             public void onClick(View v) {
+                CommonUtils.showLoading(getActivity());
                 if(!isPasswordValid(passwordEditText.getText()))
                 {
                     passwordTextInput.setError("Пароль має бути мін 8 симаолів");
