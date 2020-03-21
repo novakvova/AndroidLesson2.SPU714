25f98f3c3e875f4274770b0c57c4e454dc02bc90
novakvova <novakvova@gmail.com>
2020-03-21 11:11:34 +0200

    Add login page

--- app/src/main/java/com/example/lesson2layoutmenuadpter/LoginFragment.java
+++ app/src/main/java/com/example/lesson2layoutmenuadpter/LoginFragment.java
@@ -2,11 +2,19 @@ package com.example.lesson2layoutmenuadpter;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class LoginFragment extends Fragment {
@@ -23,7 +31,40 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_login, container, false);
 
+        Button loginButton = view.findViewById(R.id.btn_login);
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(!isPasswordValid(passwordEditText.getText()))
+                {
+                    passwordTextInput.setError("Пароль має бути мін 8 симаолів");
+                }
+                else {
+                    passwordTextInput.setError(null);
+                }
+            }
+        });
+
+        // Clear the error once more than 8 characters are typed.
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(null); //Clear the error
+                    //return true;
+                }
+                return false;
+            }
+        });
 
         return view;
+    }
+    private boolean isPasswordValid(@Nullable Editable text) {
+        return text != null && text.length() >= 8;
     }
 }
