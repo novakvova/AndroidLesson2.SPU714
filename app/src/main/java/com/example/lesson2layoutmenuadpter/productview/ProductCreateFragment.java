b302bb12583c5b3c6da0ad8e74b8f42f4566692a
novakvova <novakvova@gmail.com>
2020-04-11 11:37:36 +0300

    Choose image

--- app/src/main/java/com/example/lesson2layoutmenuadpter/productview/ProductCreateFragment.java
+++ app/src/main/java/com/example/lesson2layoutmenuadpter/productview/ProductCreateFragment.java
@@ -1,5 +1,6 @@
 package com.example.lesson2layoutmenuadpter.productview;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.lesson2layoutmenuadpter.R;
 
 public class ProductCreateFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    public static final int PICKFILE_RESULT_CODE = 1;
+    ImageView chooseImage;
+    String chooseImageBase64;
+    Button btnSelectImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,29 @@ public class ProductCreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_create, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_create, container, false);
+
+        setupViews(view);
+        setButtonSelectImageListener();
+
+        return view;
     }
+
+    private void setupViews(View view) {
+        btnSelectImage = view.findViewById(R.id.btnSelectImage);
+        chooseImage = (ImageView) view.findViewById(R.id.chooseImage);
+    }
+
+    private void setButtonSelectImageListener() {
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+                chooseFile.setType("image/*");
+                chooseFile = Intent.createChooser(chooseFile, "Оберіть малюнок продукта");
+                startActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
+            }
+        });
+    }
+
 }
